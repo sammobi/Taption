fa0546dcab4b88a87fd94ea030ffa717dc5eea16
Sam Agrawal <sam.mobi@simpalm.com>
2015-10-19 18:20:32 -0400

    Initial commit

---
+++ app/src/main/java/com/example/simpalm/taption/AddPhoneActivity.java
@@ -0,0 +1,26 @@
+package com.example.simpalm.taption;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+
+public class AddPhoneActivity extends Activity {
+
+    Button mNextBtn;
+    
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_phone);
+
+
+    }
+
+
+}
