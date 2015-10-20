2473f13f96cbd0de85dfba1d2176a4e89379b34b
Sam Agrawal <sam.mobi@simpalm.com>
2015-10-19 20:18:22 -0400

    UI updated

--- app/src/main/java/com/example/simpalm/taption/AddPhoneActivity.java
+++ app/src/main/java/com/example/simpalm/taption/AddPhoneActivity.java
@@ -1,17 +1,18 @@
 package com.example.simpalm.taption;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class AddPhoneActivity extends Activity {
 
     Button mNextBtn;
-    
-
+    TextView mPlusoneTv, mPhonenumberTv, mUnitesStatesTv;
+    private android.content.Context context;
 
 
     @Override
@@ -19,6 +20,44 @@ public class AddPhoneActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_phone);
 
+        mNextBtn = (Button) findViewById(R.id.nextbtn);
+        mPlusoneTv = (TextView) findViewById(R.id.plusonetv);
+        mPhonenumberTv = (TextView) findViewById(R.id.phonenumberet);
+        mUnitesStatesTv = (TextView) findViewById(R.id.unitedstatestv);
+
+
+        mPlusoneTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                AlertDialog.Builder alert = new AlertDialog.Builder(AddPhoneActivity.this);
+                alert.setTitle("Alert");
+                alert.setMessage("Taption is currently only available in the U.S");
+                alert.setPositiveButton("OK", null);
+                alert.show();
+            }
+        });
+
+        mUnitesStatesTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                AlertDialog.Builder alert = new AlertDialog.Builder(AddPhoneActivity.this);
+                alert.setTitle("Alert");
+                alert.setMessage("Taption is currently only available in the U.S");
+                alert.setPositiveButton("OK", null);
+                alert.show();
+
+            }
+        });
+
+        mPhonenumberTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
     }
 
