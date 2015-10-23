1cc7c4289e71433a0cd51f48ae1dcde5a5ea7b9a
Sam Agrawal <sam.mobi@simpalm.com>
2015-10-23 16:10:56 -0400

    1. Hide Native keyboard on edit text click 2. Implement onback button listener and hide custom keyboard

--- app/src/main/java/com/example/simpalm/taption/AddPhoneActivity.java
+++ app/src/main/java/com/example/simpalm/taption/AddPhoneActivity.java
@@ -2,6 +2,7 @@ package com.example.simpalm.taption;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.telephony.PhoneNumberUtils;
@@ -9,16 +10,20 @@ import android.text.Editable;
 import android.text.InputFilter;
 import android.text.TextWatcher;
 import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.Locale;
 
 public class AddPhoneActivity extends Activity {
 
-    Button mNextBtn;
+    Button mNextBtn, mONeBtn, mTwobtn, mThreeBtn, mFourBtn, mFiveBtn, mSixBtn, mSevenBtn,  mEightBtn, mNineBtn, mZeroBtn;
     TextView mPlusoneTv, mUnitesStatesTv;
+    LinearLayout mKeyboardLl;
     EditText mPhonenumberTv;
     private android.content.Context context;
 
@@ -32,7 +37,19 @@ public class AddPhoneActivity extends Activity {
         mPlusoneTv = (TextView) findViewById(R.id.plusonetv);
         mPhonenumberTv = (EditText) findViewById(R.id.phonenumberet);
         mUnitesStatesTv = (TextView) findViewById(R.id.unitedstatestv);
+        mKeyboardLl = (LinearLayout) findViewById(R.id.keyboardll);
+        mONeBtn = (Button)findViewById(R.id.btn1);
+        mTwobtn = (Button)findViewById(R.id.btn2);
+        mZeroBtn = (Button)findViewById(R.id.btn0);
 
+        mThreeBtn = (Button)findViewById(R.id.btn3);
+        mFourBtn = (Button)findViewById(R.id.btn4);
+        mFiveBtn = (Button)findViewById(R.id.btn5);
+
+        mSixBtn = (Button)findViewById(R.id.btn6);
+        mSevenBtn = (Button)findViewById(R.id.btn7);
+        mEightBtn = (Button)findViewById(R.id.btn8);
+        mNineBtn = (Button)findViewById(R.id.btn9);
         mPhonenumberTv.addTextChangedListener(new TextWatcher() {
             private boolean mFormatting; // this is a flag which prevents the  stack overflow.
             private int mAfter;
@@ -57,7 +74,7 @@ public class AddPhoneActivity extends Activity {
                     mFormatting = true;
                     // using US formatting...
                     if (mAfter != 0) // in case back space ain't clicked...
-                      PhoneNumberUtils.formatNumber(s.toString());
+                        PhoneNumberUtils.formatNumber(s.toString());
 
 
                   /*  PhoneNumber numberProto = phoneUtil.parse(numberStr, "US");
@@ -101,9 +118,50 @@ public class AddPhoneActivity extends Activity {
         mPhonenumberTv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+             mKeyboardLl.setVisibility(View.VISIBLE);
+
+                // Hide native keyboard on click of the button
+
+                InputMethodManager imm = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+                imm.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0);
 
             }
         });
+
+        mNextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                mKeyboardLl.setVisibility(View.GONE);
+
+
+            }
+        });
+
+        mONeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mPhonenumberTv.setText("1");
+
+            }
+        });
+
+    }
+
+
+
+    // back button pressed visiblity gone of certain component
+    @Override
+    public void onBackPressed() {
+
+        if (mKeyboardLl.getVisibility() == (View.VISIBLE))
+            mKeyboardLl.setVisibility(View.GONE);
+
+
+        else
+
+            super.onBackPressed();
+
 
     }
 
